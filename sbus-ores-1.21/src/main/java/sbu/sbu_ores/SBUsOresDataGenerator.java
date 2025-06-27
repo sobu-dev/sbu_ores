12ecace5fa60f1509ebff369db125037fae382a5
sobu-dev <saubhagya.srivastava.114@gmail.com>
2025-06-27 23:23:29 +0530

    Init

---
+++ sbus-ores-1.21/src/main/java/sbu/sbu_ores/SBUsOresDataGenerator.java
@@ -0,0 +1,11 @@
+package sbu.sbu_ores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SBUsOresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
