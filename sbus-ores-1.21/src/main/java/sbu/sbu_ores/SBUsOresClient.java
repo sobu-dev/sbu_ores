39496df710f93c05c4b929862b669a4270129ecf
sobu-dev <saubhagya.srivastava.114@gmail.com>
2025-06-27 23:58:53 +0530

    Work

---
+++ sbus-ores-1.21/src/main/java/sbu/sbu_ores/SBUsOresClient.java
@@ -0,0 +1,10 @@
+package sbu.sbu_ores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SBUsOresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
